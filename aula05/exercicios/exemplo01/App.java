d1fe5f8b7f7f605872ead8dcec905b1f61a0d932
sothiis <21424597@anhembimorumbi.edu.br>
2021-03-24 21:41:29 -0300

    2

--- aula05/exercicios/exemplo01/App.java
+++ aula05/exercicios/exemplo01/App.java
@@ -3,14 +3,25 @@ package exercicios.exemplo01;
 public class App {
     
     public static void main(String[] args) {
-        Placar p1 = new Placar(null, 0);
+        Placar p1 = new Placar ();
         Placar p2 = new Placar("Palmeiras", "São Paulo");
         Placar p3 = new Placar ("Palmeiras", "São Paulo", 1, 2);
 
-        
-        p1.exibir();
-        p2.exibir();
-        p3.exibir();
+
+        System.out.println( p1.toString() );
+        System.out.println( p2.toString() );
+        System.out.println( p3.toString() );
+
+
+        //System.out.println( p1.exibir() );
+        //System.out.println( p2.exibir() );
+        //System.out.println( p3.exibir() );
+
+
+        //OU
+        // p1.exibir();
+        // p2.exibir();
+        //p3.exibir();
       
 
     }
--- aula05/exercicios/exemplo01/Placar.java
+++ aula05/exercicios/exemplo01/Placar.java
@@ -6,26 +6,42 @@ public class Placar {
     int gol1, gol2;
 
 
-public Placar (String time, int resultado) { //construtor 
-    time1 = "Time da Casa";
-    time2 = "Visitante";
+//public Placar (String time, int resultado) { //construtor1 
+  //  time1 = "Time da Casa";
+   // time2 = "Visitante";
  
+//}
+//OU
+
+public Placar () { //chamando construtor 
+    this("Time da casa", "Visitante",0,0)
+; 
 }
 
-public Placar (String time1, String time2, int gol1, int gol2){
-     this.time1 = time1;
+public Placar (String time1, String time2, int gol1, int gol2){ //construtor2
+    this.time1 = time1;
     this.time2 = time2;
     this.gol1 = gol1;
     this.gol2 = gol2;
 }
 
-public Placar(String time1, String time2) {
-    this.time1 = time1;
-    this.time2 = time2;
+public Placar(String time1, String time2) { //construtor3
+   // this.time1 = time1;
+   // this.time2 = time2;
+    this (time1, time2, 0,0);
 }
 
-public void exibir() {
-    System.out.println( time1 +  gol1 + "x" +  gol2 +  time2);
-}
+//public String exibir() {
+   //     return (time1 +  gol1 + "x" +  gol2 +  time2);
+//}
+
+
+public String toString() {
+    return (time1 +  gol1 + "x" +  gol2 +  time2);
+}//
+
+//OU
+//public void exibir()
+    //System.out.println( time1 +  gol1 + "x" +  gol2 +  time2);
 
 }
